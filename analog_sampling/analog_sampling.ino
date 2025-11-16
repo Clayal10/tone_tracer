34052385d7cef0d3749ebad06db28ef71b57967c
Clay Shumaker <clayalshu@gmail.com>
2025-11-15 16:37:33 -0800

    basic LED sending and reading signal

--- analog_sampling/analog_sampling.ino
+++ analog_sampling/analog_sampling.ino
@@ -1,9 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+#define LEDPIN 2
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(LEDPIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(LEDPIN, HIGH);
+  int value = analogRead(A0);
+  Serial.println(value);
+  delay(500);
+  digitalWrite(LEDPIN, LOW);
+  value = analogRead(A0);
+  Serial.println(value);
+  delay(500);
 }
